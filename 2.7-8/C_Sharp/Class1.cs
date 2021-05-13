4c13755380d0e915c3b830c9dbe1ef88fceb5bb1
Anton2156 <akkforwork11@gmail.com>
2021-05-13 20:54:39 +0300

    2.7-8

---
+++ 2.7-8/C_Sharp/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    class ArithmeticExpressions
+    {
+        private int a, b, c, d;
+        public ArithmeticExpressions(int a, int b, int c, int d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c; 
+            this.d = d; 
+        }
+
+        public double CalculationExpressions()
+        {
+            double res = (1 + a - b / 2) / (Math.Sqrt(24 + d - c) + (a / b));
+            if ((24 + d - c) < 0)
+                throw new ArithmeticException();
+            return res;
+        }
+
+    }
+}
---
+++ 2.7-8/C_Sharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithmeticExpressions[] arr =
+            {
+                new ArithmeticExpressions(0,0,0,0),
+                new ArithmeticExpressions(1,12,242,0),
+                new ArithmeticExpressions(2,0,0,324),
+                new ArithmeticExpressions(3,123,24,23),
+                new ArithmeticExpressions(4,76,0,123),
+                new ArithmeticExpressions(5,7,42,100),
+                new ArithmeticExpressions(67,23,0,0)
+            };
+            foreach (var item in arr)
+            {
+                try
+                {
+                    Console.WriteLine("Результат:" + item.CalculationExpressions());
+                }
+                catch (DivideByZeroException)
+                {
+
+                    Console.WriteLine("Не можна ділить на ноль");
+                }
+                catch (ArithmeticException)
+                {
+
+                    Console.WriteLine("Корінь повинен бути більше 0");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
